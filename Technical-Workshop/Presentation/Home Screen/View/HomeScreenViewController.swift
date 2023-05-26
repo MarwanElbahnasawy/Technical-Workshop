3438d2660ae01959e0024b1a5b927f6e31815ed6
Marwan Elbahnasawy <96259021+MarwanElbahnasawy@users.noreply.github.com>
2023-05-26 14:55:29 +0300

    created collection view in home screen

--- Technical-Workshop/Presentation/Home Screen/View/HomeScreenViewController.swift
+++ Technical-Workshop/Presentation/Home Screen/View/HomeScreenViewController.swift
@@ -7,23 +7,49 @@
 
 import UIKit
 
-class HomeScreenViewController: UIViewController {
-
+class HomeScreenViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var categoriesCollectionView: UICollectionView!
+    @IBOutlet weak var homeTableView: UITableView!
+    
+    
+    private lazy var viewModel: HomeScreenViewModelType = {
+        return HomeScreenViewModel()
+    }()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+ 
+    // MARK: Collection View Callback Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.categoryItemsCount
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = categoriesCollectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as! CategoryCollectionViewCell
+        let currentCategoryItem = viewModel.categoryItem(at: indexPath.row)
+        cell.configure(categoryItem: currentCategoryItem)
+        
+        return cell
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = categoriesCollectionView.bounds.size.width * 0.2
+        let height = categoriesCollectionView.bounds.size.height
+        return CGSize(width: width, height: height)
+    }
+
+    // MARK: Table View Callback Methods
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 
 }
---
+++ Technical-Workshop/Presentation/Home Screen/ViewModel/HomeScreenViewModelType.swift
@@ -0,0 +1,13 @@
+//
+//  HomeScreenViewModelType.swift
+//  Technical-Workshop
+//
+//  Created by Marwan Elbahnasawy on 26/05/2023.
+//
+
+import Foundation
+
+protocol HomeScreenViewModelType{
+    var categoryItemsCount: Int { get }
+    func categoryItem(at index: Int) -> CategoryItem
+}
