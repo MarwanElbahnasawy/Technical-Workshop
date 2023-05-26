793529514cd36ad068583895819b02944dcdc4cd
Marwan Elbahnasawy <96259021+MarwanElbahnasawy@users.noreply.github.com>
2023-05-26 19:11:42 +0300

    added meals collection view

--- Technical-Workshop/Presentation/Home Screen/View/Meal Cell/MealCollectionViewCell.swift
+++ Technical-Workshop/Presentation/Home Screen/View/Meal Cell/MealCollectionViewCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class MealCollectionViewCell: UICollectionViewCell {
-   
+    
     var cellDelegate: MealCellDelgate?
     
     @IBOutlet weak var servingsLabel: UILabel!
@@ -18,14 +18,28 @@ class MealCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var recipePlaceHolder: UIImageView!
     
     override func awakeFromNib() {
+        super.awakeFromNib()
         recipePlaceHolder.makeGradient()
     }
     @IBAction func favoriteButton(_ sender: Any) {
         cellDelegate?.didPressedFavBtn()
     }
-    func setup(){
-        //TODO pass object to setup cell
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        recipePlaceHolder.layer.cornerRadius = 10
+        recipePlaceHolder.layer.masksToBounds = true
     }
+    
+    func configure(mealItem: MealItem){
+        mealRecipeLabel.text = mealItem.mealRecipe
+        chefNameLabel.text = mealItem.chefName
+        mealTypeLabel.text = mealItem.mealType
+        servingsLabel.text = mealItem.servings
+        recipePlaceHolder.image = UIImage(named: mealItem.image.rawValue)
+    }
+    
+    
 }
 protocol MealCellDelgate{
     func didPressedFavBtn()
